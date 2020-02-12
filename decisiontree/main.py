7a99dc6fec836ef4c381ff0adba646fa5bd1a439
David Durrant <ddurrant@halosight.com>
2020-02-12 12:42:10 -0700

    build tree works

--- decisiontree/main.py
+++ decisiontree/main.py
@@ -1,5 +1,7 @@
 import math
+import copy
 import numpy as np
+import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
@@ -9,99 +11,81 @@ class Node(object):
         self.child = []
         self.parent = None
         self.attribute = None
-        self.data = None
+        self.targets = None
+        self.predictions = None
 
 
 class DecisionTree:
     def fit(self, train_data, train_target):
         self.train_data = train_data
         self.train_target = train_target
-        self.attributes = list(range(0, len(train_data[0])))
-        self.node = Node()
-        self.get_node(self.train_data, dict(enumerate(train_target.flatten(), 1)), self.attributes, self.node)
+        self.attributes = train_data.columns #list(range(0, len(train_data[0])))
+        self.node = self.make_tree(self.train_data, self.train_target, self.attributes) #list(range(1, len(self.train_target) + 1)), self.attributes)
 
-    def get_node(self, data, targets, attributes, parent_node):
-        if all(target == self.train_target[targets[0]] for target in self.train_target[targets]):
-            # if all target data is the same then end branch
-            parent_node.data = self.train_target[targets[0]]
-        elif len(attributes) < 1:
-            # if there are no more attributes end branch
-            parent_node.data = self.train_target[targets[0]]
-        else:
-            # separate attributes into branches
-            branches, entropies = self.get_branches(attributes, data, targets)
+    def predict(self, test_data):
+        self.test_data = test_data
+        self.attributes = test_data.columns  # list(range(0, len(train_data[0])))
+        self.check_tree(self.train_data, self.attributes, self.node)  # list(range(1, len(self.train_target) + 1)), self.attributes)
 
-            # get lowest entropy attribute
-            parent_node.attribute = entropies.index(min(entropies))
-            parent_node.data = branches[parent_node.attribute]
+    def check_tree(self, data, features, node):
+        for row in data:
+            row[node.attribute]
 
-            # remove attribute that have been selected
-            del attributes[parent_node.attribute]
-            new_data = np.delete(data, np.s_[parent_node.attribute], axis=1)
-            for branch in branches[parent_node.attribute]:
-                # Recursive function call
-                parent_node.child.append(Node())
-                self.get_node(new_data, branch, attributes, parent_node.child[len(parent_node.child)-1])
+    def make_tree(self, data, targets, features):
+        node = Node()
+        node.targets = targets
+        # for value in targets:
+        #     target_values.append(self.train_target[value])
+        if all(item == targets[0] for item in targets) or (len(features) == 0):
+            return node
 
-    def get_branches(self, attributes, data, target):
-        attribute_indices = []
-        branch_entropy = []
+        feature_targets = self.get_feature_branches(data, targets, features)
+        info_gain = self.get_feature_entropy(feature_targets, targets)
+        index_min = np.argmin(info_gain)
+        node.attribute = features[index_min]
 
-        # for each column
-        for label in range(len(attributes)):
-            column = data[:, label]
-            branch_indices = []
+        rem_data = copy.deepcopy(data)
+        rem_data = rem_data.drop([node.attribute], axis=1) #np.delete(rem_data, node.attribute, 1)
+        rem_features = rem_data.columns
+        for branch in feature_targets[index_min]:
+            node.child.append(self.make_tree(rem_data, branch, rem_features))
 
-            # for each unique value in column
+        return node
+
+    def get_feature_branches(self, data, targets, features):
+        all = []
+        for feature in features:
+            column = data.loc[:, feature]
+            feature_targets = []
             for branch in set(column):
-                indices = []
+                branch_targets = []
                 for value in range(len(column)):
-                    if np.all((value in target) & (column[value] == branch)):
+                    if np.all(((value + 1) in targets) & (column[value] == branch)):
                         # get indices of each value
-                        indices.append(value)
-                branch_indices.append(indices)
+                        branch_targets.append(value + 1)
+                feature_targets.append(branch_targets)
+            all.append(feature_targets)
+        return all
 
-            # add branch indices
-            attribute_indices.append(branch_indices)
-            # get target value connected to branch_indices
-            branch_values = self.get_targets(branch_indices)
-
-            set_entropy = []
-            splits = []
-            # for each branch in attribute
-            for values in branch_values:
-                # get occurrence of each value
-                value_count = np.bincount(values)
-                entropy = []
-                # for each unique value in branch
-                for value in value_count:
-                    # get value count / total values
-                    entropy.append(value / len(values))
-                # get entropy of branch
-                set_entropy.append(self.entropy(entropy))
-                splits.append(len(values) / len(column))
-
-            field_entropy = []
-            # for each branch entropy
-            for entropy, split in zip(set_entropy, splits):
-                # values in branch / total values in parent node
-                field_entropy.append([split, entropy])
-            # get information gain
-            branch_entropy.append(self.gain(field_entropy))
-
-        # return the indices connected to each attributes branches and entropy
-        return attribute_indices, branch_entropy
-
-    def get_targets(self, branch):
-        values = []
-        for attribute in branch:
-            values.append(target[attribute])
-        return values
-
+    def get_feature_entropy(self, feature_targets, target_values):
+        info_gain = []
+        for feature in feature_targets:
+            feature_prep = []
+            for branch in feature:
+                target_branch = [self.train_target[i] for i in branch]  # target_values[branch]
+                # for item in branch:
+                #     target_branch.append(self.train_target[item])
+                branch_prep = []
+                for count in np.bincount(target_branch):
+                    branch_prep.append(count / len(branch))
+                feature_prep.append(len(branch) / len(target_values))
+                feature_prep.append(self.calc_entropy(branch_prep))
+            info_gain.append(self.calc_info_gain(feature_prep))
+        return info_gain
 
     # get the entropy of the values in decision
     # values = (value_x_count / total_values) foreach value in category
-    def entropy(self, values):
+    def calc_entropy(self, values):
         score = 0
         for v in values:
             if v == 0:
@@ -113,33 +97,58 @@ class DecisionTree:
 
     # get the entropy of the sets from category
     # values = [(category_x_count / total_category_values), set_entropy_score] foreach category
-    def gain(self, values):
+    def calc_info_gain(self, values):
         score = 0
-        for value in values:
-            score += (value[0] * value[1])
-
+        i = 0
+        while i < len(values):
+            score += (values[i] * values[i + 1])
+            i += 2
         return score
 
 
-# iris = datasets.load_iris()
-iris = np.array([[2, 1, 1],
-                 [2, 1, 0],
-                 [2, 0, 1],
-                 [2, 0, 0],
-                 [1, 1, 1],
-                 [1, 0, 0],
-                 [1, 1, 0],
-                 [1, 0, 1],
-                 [0, 1, 1],
-                 [0, 1, 0],
-                 [0, 0, 1],
-                 [0, 0, 0]])
-target = np.array([1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0])
-# iris_train, iris_test, target_train, target_test = \
-#     train_test_split(iris, target, test_size=0.00, train_size=1.00, shuffle=False)
+car_header = ["buying", "maint", "doors", "persons", "lug_boot", "safety", "class"]
+car_cleanup_data = {"buying": {"vhigh": 4, "high": 3, "med": 2, "low": 1},
+                    "maint": {"vhigh": 4, "high": 3, "med": 2, "low": 1},
+                    "doors": {"5more": 5},
+                    "persons": {"more": 6},
+                    "lug_boot": {"small": 1, "med": 2, "big": 3},
+                    "safety": {"low": 1, "med": 2, "high": 3},
+                    "class": {"vgood": 4, "good": 3, "acc": 2, "unacc": 2}}
+
+car = pd.read_csv("data/car.data", names=car_header)
+
+obj_car = car.select_dtypes(include=['object']).copy()
+obj_car.replace(car_cleanup_data, inplace=True)
+
+x = obj_car.drop(["class"], axis=1)
+y = obj_car["class"]
+
+iris_train, iris_test, target_train, target_test = \
+    train_test_split(x, y, test_size=0.30, train_size=0.70, shuffle=False)
 
 classifier = DecisionTree()
-classifier.fit(iris, target)
-print(classifier.node.data)
-print(classifier.node.child[0].data, "  -  ", classifier.node.child[0].data[1])
+classifier.fit(iris_train, target_train)
+classifier.predict(iris_test)
+print(classifier.node.targets)
+
+# iris = datasets.load_iris()
+# iris = np.array([[2, 1, 1],
+#                  [2, 1, 0],
+#                  [2, 0, 1],
+#                  [2, 0, 0],
+#                  [1, 1, 1],
+#                  [1, 0, 0],
+#                  [1, 1, 0],
+#                  [1, 0, 1],
+#                  [0, 1, 1],
+#                  [0, 1, 0],
+#                  [0, 0, 1],
+#                  [0, 0, 0]])
+# target = np.array([1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0])
+# iris_train, iris_test, target_train, target_test = \
+#     train_test_split(iris.data, iris.target, test_size=0.30, train_size=0.70, shuffle=False)
+#
+# classifier = DecisionTree()
+# classifier.fit(iris.data, iris.target)
+# print(classifier.node.targets)
 # predictions = classifier.predict(iris_test)
